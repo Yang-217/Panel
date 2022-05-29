b11657002f67ee9401873c58a8a61dc3befbbc80
Yang-217 <71708272+Yang-217@users.noreply.github.com>
2022-05-29 13:36:52 +0800

    Update lmt.js

--- surge/JS/lmt.js
+++ surge/JS/lmt.js
@@ -38,7 +38,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       title: '流媒体解锁检测',
       content: '',
       icon: '4k.tv.fill',
-      'icon-color': '#53FF53',
+      'icon-color': '#00FF00',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
