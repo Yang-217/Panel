bca55d3c8a874e296f215875c3f05e101b500be5
Yang-217 <71708272+Yang-217@users.noreply.github.com>
2022-05-30 22:11:03 +0800

    Update sub_info.js

--- surge/JS/sub_info.js
+++ surge/JS/sub_info.js
@@ -136,7 +136,8 @@ function bytesToSize(bytes) {
   let k = 1024;
   sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let i = Math.floor(Math.log(bytes) / Math.log(k));
-  return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
+  #return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
+  return (bytes / Math.pow(k, i)*100).toFixed(2) + " %" ;
 }
 
 function formatTime(time) {
